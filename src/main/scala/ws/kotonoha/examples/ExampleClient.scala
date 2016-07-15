055445b7af5e706a3b13dcdf320b0a7c9a5e7b9e
Arseny Tolmachev <eiennohito@gmail.com>
2016-07-15 16:12:23 +0900

    add client interface & factory

---
+++ src/main/scala/ws/kotonoha/examples/ExampleClient.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 eiennohito (Tolmachev Arseny)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ws.kotonoha.examples
+
+import io.grpc.{CallOptions, Channel}
+import org.eiennohito.grpc.stream.client._
+import ws.kotonoha.examples.api.{ExamplePack, ExamplePackRequest, KotonohaExamplesGrpc}
+
+/**
+  * @author eiennohito
+  * @since 2016/07/15
+  */
+trait ExampleClient extends AStreamClient {
+  def generic: UnaryCall[ExamplePackRequest, ExamplePack]
+}
+
+object ExampleClient extends AClientCompanion[ExampleClient] {
+  override def name = KotonohaExamplesGrpc.descriptor.getName
+  override def build(channel: Channel, callOptions: CallOptions) = {
+    val bldr = ClientBuilder(channel, callOptions)
+    new ExampleClient {
+      override def generic = bldr.unary(KotonohaExamplesGrpc.METHOD_GENERIC)
+    }
+  }
+}
